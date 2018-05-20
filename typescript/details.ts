a3d13b8cd9a996efd45e922b468b2ffd5f4bd624
aniruddha <aniruddha.kudalkar@gmail.com>
2018-05-20 10:07:24 +0530

    understanding the types and classes

---
+++ typescript/details.ts
@@ -0,0 +1,17 @@
+export class Details {
+    tms: number
+    nm: string
+    age: number
+}
+
+let dt: Details = new Details()
+
+let obj = {
+    tms : 456546,
+    nm : 'android',
+    age : 5,
+    sm : 56
+}
+
+dt = obj 
+
---
+++ typescript/intro.ts
@@ -0,0 +1,19 @@
+let nm = 'android'
+let num: number = 56
+let sub: number | boolean | string = true
+let fn: (mob: string) => void = function (mob: string) {
+
+}
+
+let obj: { [key: string]: string | number } = { 
+    age : 10,
+    nm : 'android'
+}
+
+function dollar(): number {
+    return 89
+}
+
+function currency(val: number, cur: string): void {
+
+}
